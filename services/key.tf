fd7c57ae0b828f6e6619ddd68f89b2d1fc6c08d9
tindd1810 <duytin1810@gmail.com>
2022-10-04 10:58:17 +0700

    .

--- services/key.tf
+++ services/key.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "prod-publickey" {
   key_name = "tindd-publickey"
-  public_key = file("${path.module}/prod-publickey")
+  public_key = file("${path.module}/ss-publickey")
 }
