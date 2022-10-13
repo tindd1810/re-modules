7886e8d37aa6950eb3615d4beda6d60dcbae6432
tindd1810 <duytin1810@gmail.com>
2022-10-13 10:32:08 +0700

    .

--- services/userdata.sh
+++ services/userdata.sh
@@ -2,6 +2,7 @@
 apt update
 # apt install nginx -y
 # apt install docker.io -y
+apt install net-tools -y
 # systemctl start nginx
 # echo $(curl http://169.254.169.254/latest/meta-data/public-ipv4) > /var/www/html/index.nginx-debian.html
 apt install ansible -y
