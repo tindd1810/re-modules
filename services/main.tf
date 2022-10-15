1ab278a07b8a399d09bea378bb621a2af109b9b4
tindd1810 <duytin1810@gmail.com>
2022-10-15 13:54:54 +0700

    .

--- services/main.tf
+++ services/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "tindd-amz-ec2" {
   count                  = var.ec2-instance
   ami                    = "ami-04ff9e9b51c1f62ca"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   # vpc_security_group_ids = [aws_security_group.tindd-re-sg.id]
   vpc_security_group_ids = [data.terraform_remote_state.networking.outputs.sg-id]
   key_name               = aws_key_pair.prod-publickey.key_name
@@ -28,12 +28,15 @@ data "terraform_remote_state" "networking" {
 output "ec2-ip0" {
   value = aws_instance.tindd-amz-ec2[0].public_ip
 }
-# output "ec2-ip1" {
-#   value = aws_instance.tindd-amz-ec2[1].public_ip
-# }
-# output "ec2-ip2" {
-#   value = aws_instance.tindd-amz-ec2[2].public_ip
-# }
+output "ec2-ip1" {
+  value = aws_instance.tindd-amz-ec2[1].public_ip
+}
+output "ec2-ip2" {
+  value = aws_instance.tindd-amz-ec2[2].public_ip
+}
+output "ec2-ip3" {
+  value = aws_instance.tindd-amz-ec2[3].public_ip
+}
 # output "tindd-ec2-ip" {
 #     value = aws_instance.tindd-amz-ec2.public_ip
 # }
--- services/userdata.sh
+++ services/userdata.sh
@@ -5,4 +5,4 @@ apt update
 apt install net-tools -y
 # systemctl start nginx
 # echo $(curl http://169.254.169.254/latest/meta-data/public-ipv4) > /var/www/html/index.nginx-debian.html
-apt install ansible -y
+# apt install ansible -y
