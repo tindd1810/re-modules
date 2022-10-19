ae31ec5ef4cd8414de8e8348af442d39715d1007
tindd1810 <duytin1810@gmail.com>
2022-10-19 14:10:54 +0700

    .

--- jenkins-node/userdata.sh
+++ jenkins-node/userdata.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 apt update -y
-# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-# unzip awscliv2.zip
-# ./aws/install
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
 apt install default-jre -y
 apt install git -y
 apt install ansible -y
