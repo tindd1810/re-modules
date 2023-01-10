9388e7a3a54308a646595d9d93437bd8549d578f
tindd1810 <duytin1810@gmail.com>
2023-01-10 10:05:28 +0700

    update for kubespray

--- services/userdata.sh
+++ services/userdata.sh
@@ -5,4 +5,8 @@ apt update
 apt install net-tools -y
 # systemctl start nginx
 # echo $(curl http://169.254.169.254/latest/meta-data/public-ipv4) > /var/www/html/index.nginx-debian.html
-# apt install ansible -y
+apt install ansible -y
+apt install software-properties-common -y
+add-apt-repository ppa:deadsnakes/ppa -y
+apt update
+apt install python3.8 -y
